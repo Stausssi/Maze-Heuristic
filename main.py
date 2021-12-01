174892a740052af7fce1178470dcb3aa786e6232
n1klasD <67094918+n1klasD@users.noreply.github.com>
2021-12-01 20:13:25 +0100

    tests

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from util import BoardHelper
 
 
 def main():
-    boardIndex = 1
+    boardIndex = 2
+
+    # heuristic_floor_euclid: 1 --> 8 Moves (50k Open)
+    # heuristic_floor_euclid: 2 --> 11 Moves (9k Open)
+
+    # heuristic_shortest_distance_end_path_player_path: 1 --> 8 Moves (11k Open)
+    # heuristic_shortest_distance_end_path_player_path: 2 --> 10 Moves (7k Open)
 
     field, spareTile = BoardHelper.readBoardFromCSV(f"data/puzzle_{boardIndex}.csv")
     startColumn, endColumn = BoardHelper.readBoardInformation(f"data/info_{boardIndex}.txt")
